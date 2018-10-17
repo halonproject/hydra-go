84b6850a1ed5240bce284aa07f808a574be0c8d8
Chris Purta <cpurta@kochava.com>
2018-10-17 12:26:22 -0700

    echo error message and echo git url for debugging

--- scripts/report_error.sh
+++ scripts/report_error.sh
@@ -12,6 +12,8 @@ function report_error_to_github {
 
     ERROR_MSG=$(cat $ERROR_LOG)
 
+    echo $ERROR_MSG
+
     post_error_to_github
 }
 
@@ -22,6 +24,8 @@ function get_pull_request_diff {
 }
 
 function post_error_to_github {
+    echo "posting error message to https://api.github.com/repos/$PROJECT_OWNER/$CIRCLE_PR_REPONAME/pulls/$CIRCLE_PR_NUMBER/comments"
+
     curl --request POST \
   --url https://api.github.com/repos/$PROJECT_OWNER/$CIRCLE_PR_REPONAME/pulls/$CIRCLE_PR_NUMBER/comments \
   --header 'accept: application/vnd.github.v3+json' \
